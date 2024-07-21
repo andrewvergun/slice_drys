48f2e4ff755275c1065416dc184e4986cc62e573
olegtkach <olegontkach101@gmail.com>
2024-07-21 14:27:38 +0300

    feat: start proj

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import createNextIntlPlugin from "next-intl/plugin";
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        formats: ["image/webp"],
+        remotePatterns: [
+        {
+            protocol: "https",
+            hostname: "res.cloudinary.com",
+            port: "",
+        },
+        ],
+    },
+};
+
+export default withNextIntl(nextConfig);
