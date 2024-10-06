00aa94c7a585773a39e4a6707ae2b30786c0059b
Chaooohs <01nikka01@gmail.com>
2024-10-06 09:28:18 +0300

    feat: add animation

--- src/components/header-info.tsx
+++ src/components/header-info.tsx
@@ -1,12 +1,26 @@
+'use client'
+import { useRef } from 'react'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(useGSAP)
 
 type Props = {
   title: string
 }
 
 export default function Info({ title }: Props) {
+  const infoRef = useRef<HTMLDivElement>(null)
+
+  useGSAP(() => {
+    gsap.fromTo(infoRef.current, { xPercent: -100 }, { xPercent: 0, duration: 1 })
+  })
+
   return (
-    <div className='h-8 bg-black absolute top-0 left-0 w-full'>
-      <h1 className='text-white font-medium text-base text-center leading-[180%]'>{title}</h1>
+    <div ref={infoRef} className="absolute z-40 left-0 top-0 h-8 w-full bg-black">
+      <h1 className="text-center text-base font-medium leading-[180%] text-white">
+        {title}
+      </h1>
     </div>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,10 @@
+'use client'
+import { useRef } from 'react'
+import { FC } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FC } from 'react'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 import facebookIcon from '/public/icons/facebook.svg'
 import instagramIcon from '/public/icons/instagram.svg'
@@ -11,11 +15,20 @@ import cartIcon from '/public/icons/bin.svg'
 import Lang from './lang'
 import Info from './header-info'
 
+gsap.registerPlugin(useGSAP)
+
+
 const Header: FC = () => {
+  const headerRef = useRef<HTMLDivElement>(null)
+
+  useGSAP(() => {
+    gsap.fromTo(headerRef.current, { yPercent: -200 }, { yPercent: 0, delay: 1, duration: 1 })
+  })
+
   return (
     <header>
-      <Info title='Безкоштовна доставка від 1000 грн.'/>
-      <div className="box-border max-w-[1240px] mx-auto mt-8">
+      <Info title='Безкоштовна доставка від 1000 грн.' />
+      <div ref={headerRef} className="box-border max-w-[1240px] mx-auto mt-8">
         <div className="grid h-40 grid-cols-[1fr_2fr_1fr_1fr] grid-rows-2 border-b border-[#e4e4e4] pb-6">
           <nav className="grid grid-cols-[135px_97px_65px_48px] items-center justify-items-center gap-x-2 text-xl">
             <Link href="#!" className="justify-self-start flex items-center gap-x-4">
