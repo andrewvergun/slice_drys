ac4bb7ad3f3f7157ed3d1b6cc0004ee84c24c4b5
dromse <dromasko4@gmail.com>
2024-12-03 16:20:52 +0200

    feat: adaptive arc width with menu items

--- src/components/client/hero/hero.tsx
+++ src/components/client/hero/hero.tsx
@@ -50,55 +50,12 @@ export const Hero = () => {
   })
 
   const hoverHexColor = sliders[hoveredIndex].color
-  const hoverTransHexColor = `${hoverHexColor}4D`!
-
-  const SliderText = ({
-    item,
-    index,
-  }: {
-    index: number
-    item: { name: string }
-  }) => (
-    <div
-      className={cn(
-        'z-20 text-[36px] blur-0 hover:text-[#A90909]',
-        hoveredIndex === index && 'text-[#A90909]',
-      )}
-    >
-      <svg
-        width="200"
-        height="200"
-        viewBox="0 0 300 300"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <path
-            id="curve"
-            d="M 50,150 A 350,350 0 0,1 250,150"
-            fill="transparent"
-            stroke="transparent"
-          />
-        </defs>
-
-        <text
-          fontSize={hoveredIndex === index ? 36 : 20}
-          fontFamily="Arial"
-          letterSpacing="2"
-          fill={hoveredIndex === index ? hoverHexColor : 'black'}
-        >
-          <textPath href="#curve" startOffset="50%" textAnchor="middle">
-            {item.name}
-          </textPath>
-        </text>
-      </svg>
-    </div>
-  )
 
   return (
     <div className="mx-auto max-w-[1280px] px-[20px] pt-9">
       <div
         className={cn(
-          'relative w-fit origin-left -rotate-[2.92deg] transform text-[64px] font-bold text-white',
+          'relative w-fit origin-left -rotate-[2.92deg] transform text-[18px] font-bold text-white sm:text-[64px]',
         )}
         ref={titleRef}
       >
@@ -112,82 +69,149 @@ export const Hero = () => {
         />
       </div>
 
-      <nav className="mt-[50px] flex justify-around">
+      <nav className="relative mt-12 flex w-full justify-around">
         {sliderLinks.map((item, index) => (
-          <div key={item.name} className="relative">
+          <div
+            key={item.name}
+            className={cn(
+              'absolute bottom-0 h-[200%] translate-y-1/2',
+              index === 0 && '-rotate-[50deg]',
+              index === 1 && '-rotate-[15deg]',
+              index === 2 && 'rotate-[15deg]',
+              index === 3 && 'rotate-[50deg]',
+            )}
+          >
             <Link
               href={item.link}
               className={cn(
-                'relative z-10 flex size-[180px] items-center justify-center rounded-full text-[20px] text-[#9B9B9B] transition-colors duration-300',
-                index === 0 && 'absolute -ml-[12px] mt-[50px] rotate-[-42deg]',
-                index === 1 &&
-                  'absolute -ml-[102px] -mt-[75px] rotate-[-17deg]',
-                index === 2 && 'absolute -mt-[43px] rotate-[26deg]',
-                index === 3 &&
-                  'absolute -right-[38px] mt-[80px] rotate-[49deg]',
+                'relative left-1/2 flex size-[180px] -translate-x-1/2 -translate-y-[55%] items-center justify-center rounded-full text-[20px] text-[#9B9B9B] transition-colors duration-300',
               )}
               onMouseEnter={() => handleMouseEnter(index)}
             >
-              <SliderText index={index} item={item} />
-
-              <div
-                className={cn(
-                  'absolute mt-5 size-2 bg-black',
-                  hoveredIndex === index && 'size-5 rounded-full',
-                )}
-                style={
-                  hoveredIndex === index ? { background: hoverHexColor } : {}
-                }
-              />
-
-              <div
-                style={{ background: hoverTransHexColor }}
-                className={cn(
-                  `opacity-1 absolute h-full w-full blur-3xl`,
-                  hoveredIndex !== index && 'opacity-0',
-                )}
+              <SliderItem
+                index={index}
+                item={item}
+                hoveredIndex={hoveredIndex}
               />
             </Link>
           </div>
         ))}
+
+        <div className="relative -z-10 mx-auto w-full max-w-[1104px]">
+          <Arcs color={hoverHexColor} />
+
+          <div className="absolute -bottom-16 right-1/2 h-2/3 w-1/3 translate-x-1/2">
+            <Image
+              src={sliders[hoveredIndex].image}
+              alt="slider image"
+              fill
+              className="object-contain"
+            />
+          </div>
+        </div>
       </nav>
-
-      <div className="relative mx-auto h-[552px] w-full max-w-[1104px]">
-        <svg width="1106" height="553" viewBox="0 0 1106 553" fill="none">
-          <path
-            d="M1105 553C1105 406.601 1046.84 266.197 943.323 162.677C839.803 59.157 699.4 1.00001 553 1C406.601 0.999989 266.197 59.157 162.677 162.677C59.157 266.197 1.00002 406.6 1 553"
-            stroke="url(#paint0_linear_158_1319)"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_158_1319"
-              x1="1078.5"
-              y1="527"
-              x2="116"
-              y2="229.5"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#9B9B9B" stop-opacity="0.1" />
-              <stop offset="0.961123" stop-color={hoverHexColor} />
-            </linearGradient>
-          </defs>
-        </svg>
-
-        <Image
-          src="/icons/slider-line-down.svg"
-          className="-z-20 mt-[33px] px-[62px]"
-          fill={true}
-          alt="slider"
-        />
-
-        <Image
-          src={sliders[hoveredIndex].image}
-          className="absolute -bottom-16 right-1/2 translate-x-1/2"
-          width={480}
-          height={420}
-          alt="slider image"
-        />
-      </div>
     </div>
   )
 }
+
+const SliderItem = ({
+  item,
+  index,
+  hoveredIndex,
+}: {
+  index: number
+  item: { name: string }
+  hoveredIndex: number
+}) => {
+  const hoverColor = sliders[hoveredIndex].color
+  const hoverBlurColor = `${sliders[hoveredIndex].color}4D`
+
+  return (
+    <>
+      <div
+        className={cn(
+          'z-20 text-[36px] blur-0 hover:text-[#A90909]',
+          hoveredIndex === index && 'text-[#A90909]',
+        )}
+      >
+        <svg
+          width="200"
+          height="200"
+          viewBox="0 0 300 300"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <path
+              id="curve"
+              d="M 50,150 A 350,350 0 0,1 250,150"
+              fill="transparent"
+              stroke="transparent"
+            />
+          </defs>
+
+          <text
+            fontSize={hoveredIndex === index ? 36 : 20}
+            fontFamily="Arial"
+            letterSpacing="2"
+            fill={hoveredIndex === index ? hoverColor : 'black'}
+          >
+            <textPath href="#curve" startOffset="50%" textAnchor="middle">
+              {item.name}
+            </textPath>
+          </text>
+        </svg>
+      </div>
+
+      {/* bullet */}
+      <div
+        className={cn(
+          'absolute mt-5 size-2 bg-black',
+          hoveredIndex === index && 'size-5',
+        )}
+        style={hoveredIndex === index ? { background: hoverColor } : {}}
+      />
+
+      {/* blur */}
+      <div
+        style={{ background: hoverBlurColor }}
+        className={cn(
+          `opacity-1 absolute h-full w-full blur-3xl`,
+          hoveredIndex !== index && 'opacity-0',
+        )}
+      />
+    </>
+  )
+}
+
+const Arcs = ({ color }: { color: string }) => (
+  <>
+    <svg width="100%" height="100%" viewBox="0 0 1106 553" fill="none">
+      <path
+        id="arcPath"
+        d="M1105 553C1105 406.601 1046.84 266.197 943.323 162.677C839.803 59.157 699.4 1.00001 553 1C406.601 0.999989 266.197 59.157 162.677 162.677C59.157 266.197 1.00002 406.6 1 553"
+        stroke="url(#paint0_linear_158_1319)"
+      />
+
+      <defs>
+        <linearGradient
+          id="paint0_linear_158_1319"
+          x1="1078.5"
+          y1="527"
+          x2="116"
+          y2="229.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#9B9B9B" stopOpacity="0.1" />
+          <stop offset="0.961123" stopColor={color} />
+        </linearGradient>
+      </defs>
+    </svg>
+
+    <Image
+      src="/icons/slider-line-down.svg"
+      className="-z-20 mt-[33px] px-[62px]"
+      fill={true}
+      alt="slider"
+    />
+  </>
+)
