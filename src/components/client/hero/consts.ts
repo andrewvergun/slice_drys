f2a3df1c55ff3752f63d9226f107810bd82e7156
dromse <dromasko4@gmail.com>
2024-12-06 23:05:47 +0200

    remove 'що вражають'

--- src/components/client/hero/consts.ts
+++ src/components/client/hero/consts.ts
@@ -5,27 +5,33 @@ export const sliderLinks = [
   { name: 'ФРУКТИ', link: '/products/fruits' },
   { name: 'ОВОЧІ', link: '/products/vegetables' },
   { name: 'МІКСИ', link: '/products/mix' },
+  { name: 'АКЦІЯ', link: '/products/promo' },
 ]
 
 export const sliders = [
   {
-    title: "М'ясні сушені, що вражають",
+    title: "М'ясні сушені",
     image: '/sliders/meat.png',
     color: colorConstants.red,
   },
   {
-    title: 'Фруктові сушені, що вражають',
+    title: 'Фруктові сушені',
     image: '/sliders/fruit.png',
     color: colorConstants.orange,
   },
   {
-    title: 'Овочеві сушені, що вражають',
+    title: 'Овочеві сушені',
     image: '/sliders/veggie.png',
     color: colorConstants.green,
   },
   {
-    title: 'Мікс сушені, що вражають',
+    title: 'Мікс сушені',
     image: '/sliders/mix.png',
     color: colorConstants.purple,
+  },
+  {
+    title: 'Акційна пропозиція',
+    image: '/sliders/promo.png',
+    color: colorConstants.red,
   },
 ]
